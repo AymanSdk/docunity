d6d643727c61e4ef4685754b93a2618db1115983
Aymansdk <ayman.sdk123@outlook.com>
2024-07-21 17:23:57 +0100

    refactor: Update CollaborativeRoom component to include document title editing functionality

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Editor } from "@/components/editor/Editor";
 import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getDocument } from "@/lib/actions/room.actions";
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  // TODO: Assess the permissions of the user accessing the document
+
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom />
+      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
     </main>
   );
 };
