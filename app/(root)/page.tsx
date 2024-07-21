9af5b0856bdc65122643dd41f8b59d21f995774b
Aymansdk <ayman.sdk123@outlook.com>
2024-07-21 18:00:10 +0100

    refactor: Update Home component to include document list with metadata and create document button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import AddDocumentBtn from "@/components/AddDocumentBtn";
-import Header from "@/components/Header";
 import { getDocuments } from "@/lib/actions/room.actions";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { dateConverter } from "@/lib/utils";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -31,7 +33,37 @@ const Home = async () => {
           <div className="document-list-container">
             <div className="document-list-title">
               <h3 className="text-28-semibold">All documents</h3>
+              <AddDocumentBtn
+                userId={clerkUser.id}
+                email={clerkUser.emailAddresses[0].emailAddress}
+              />
             </div>
+            <ul className="document-ul">
+              {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+                <li key={id} className="document-list-item">
+                  <Link
+                    href={`/documents/${id}`}
+                    className="flex flex-1 items-center gap-4"
+                  >
+                    <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                      <Image
+                        src="/assets/icons/doc.svg"
+                        alt="file"
+                        width={40}
+                        height={40}
+                      />
+                    </div>
+                    <div className="space-y-1">
+                      <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                      <p className="text-sm font-light text-blue-100">
+                        Created about {dateConverter(createdAt)}
+                      </p>
+                    </div>
+                  </Link>
+                  {/* TODO: ADD a delete button */}
+                </li>
+              ))}
+            </ul>
           </div>
         ) : (
           <div className="document-list-empty">
