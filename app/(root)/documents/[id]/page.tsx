1b66989e388a8afa7f2a655816a665ff66b584d9
Aymansdk <ayman.sdk123@outlook.com>
2024-07-21 21:42:46 +0100

    refactor: Update CollaborativeRoom component to include users data and current user type

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,6 +3,7 @@ import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUser } from "@/lib/actions/user.actions";
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
   const clerkUser = await currentUser();
@@ -15,11 +16,30 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   if (!room) redirect("/");
 
-  // TODO: Assess the permissions of the user accessing the document
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUser({ userIds });
+
+  const usersData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes("room:write")
+      ? "editor"
+      : "viewer",
+  }));
+
+  const currentUserType = room.usersAccesses[
+    clerkUser.emailAddresses[0].emailAddress
+  ]?.includes("room:write")
+    ? "editor"
+    : "viewer";
 
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        users={usersData}
+        currentUserType={currentUserType}
+      />
     </main>
   );
 };
