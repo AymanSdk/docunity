a94feaf24952ac8ac787527ff1fde071be815cf8
Aymansdk <ayman.sdk123@outlook.com>
2024-07-21 17:46:56 +0100

    feat: Add getDocuments function to fetch room documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
+import { getDocuments } from "@/lib/actions/room.actions";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
@@ -10,7 +11,10 @@ const Home = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect("/sign-in");
 
-  const documents = [];
+  const roomDocuments = await getDocuments(
+    clerkUser.emailAddresses[0].emailAddress
+  );
+
   return (
     <div>
       <main className="home-container">
@@ -23,8 +27,12 @@ const Home = async () => {
           </div>
         </Header>
 
-        {documents.length > 0 ? (
-          <div></div>
+        {roomDocuments.data.length > 0 ? (
+          <div className="document-list-container">
+            <div className="document-list-title">
+              <h3 className="text-28-semibold">All documents</h3>
+            </div>
+          </div>
         ) : (
           <div className="document-list-empty">
             <Image
