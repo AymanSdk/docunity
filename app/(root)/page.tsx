ee4b75f083030cda613c04ada09f20d85e9eb22b
Aymansdk <ayman.sdk123@outlook.com>
2024-07-22 22:16:28 +0100

    feat: Add Notifications component and Popover UI for displaying inbox notifications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ import Header from "@/components/Header";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Notifications from "@/components/Notifications";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -23,7 +24,7 @@ const Home = async () => {
       <main className="home-container">
         <Header className="sticky left-0 top-0">
           <div className="flex items-center gap-2 lg:gap-4">
-            Notification
+            <Notifications />
             <SignedIn>
               <UserButton />
             </SignedIn>
